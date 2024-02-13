2bf56aec1e4e9b9bc93b54d2455d0689f9ea9a7d
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-12 20:51:23 -0500

    Work on TriviaItem component
    Along with writing logic for managing the data corresponding to the TriviaItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,76 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Intro from "./components/intro/Intro";
+import TriviaItem from "./components/question/TriviaItem";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [triviaItems, setTriviaItems] = useState([]);
+
+  useEffect(() => {
+    const stuff = fetch(
+      "https://opentdb.com/api.php?amount=5&category=10&difficulty=easy&type=multiple"
+    )
+      .then((res) => res.json())
+      .then((json) => transformResponse(json))
+      .then((items) => setTriviaItems(items));
+  }, []);
+
+  function generateOptionsObjects(optionsArr) {
+    const randomizeArray = (arr) => arr.sort((a, b) => 0.5 - Math.random());
+    return randomizeArray(
+      optionsArr.map((optionText) => ({
+        id: nanoid(),
+        text: optionText,
+        selected: false,
+      }))
+    );
+  }
+
+  function setSelectedOption(itemId, optionId) {
+    const setSelected = (options) =>
+      options.map((option) =>
+        option.id === optionId
+          ? { ...option, selected: true }
+          : { ...option, selected: false }
+      );
+
+    setTriviaItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId
+          ? { ...item, options: setSelected(item.options, optionId) }
+          : item
+      )
+    );
+  }
+
+  function transformResponse(response) {
+    return response.results.map((triviaItem) => ({
+      id: nanoid(),
+      question: triviaItem.question,
+      options: generateOptionsObjects([
+        ...triviaItem.incorrect_answers,
+        triviaItem.correct_answer,
+      ]),
+      correctAnswer: triviaItem.correct_answer,
+      selected: false,
+    }));
+  }
+
+  const triviaItemElems = triviaItems.map((item) => (
+    <TriviaItem
+      key={item.id}
+      question={item.question}
+      options={item.options}
+      selected={item.selected}
+      setSelected={(optionId) => setSelectedOption(item.id, optionId)}
+    />
+  ));
 
   return (
     <>
+      {/* <Intro /> */}
+      {triviaItemElems}
     </>
   );
 }
---
+++ src/components/question/TriviaItem.jsx
@@ -0,0 +1,24 @@
+import { nanoid } from "nanoid";
+import "./trivia-item.css";
+
+export default function TriviaItem(props) {
+  const optionElems = props.options.map((option) => (
+    <button
+      className={`trivia-item__option ${
+        option.selected && "trivia-item__option_selected"
+      }`}
+      key={nanoid()}
+      onClick={() => props.setSelected(option.id)}
+    >
+      {option.text}
+    </button>
+  ));
+
+  return (
+    <div className="question">
+      <h1 className="trivia-item__title">{props.question}</h1>
+      <div className="trivia-item__options-container">{optionElems}</div>
+      <hr className="trivia-item__line"></hr>
+    </div>
+  );
+}
