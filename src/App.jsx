10db401918d2ad020ccbe9a1d1acaf2b59923f21
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-13 11:06:59 -0500

    Add BrowserRouter to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Quiz from "./pages/quiz/Quiz";
 
 function App() {
   return (
-    <>
-      <Quiz />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
