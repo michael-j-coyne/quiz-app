eb813122412bfa3cbcf36610dec706ba255a68d9
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-13 11:05:17 -0500

    Move "quiz" part of the app into its own component

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes></Routes>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
---
+++ src/pages/quiz/Quiz.jsx
@@ -0,0 +1,84 @@
+import TriviaItem from "../../components/trivia-item/TriviaItem";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import { decode } from "html-entities";
+import "./quiz.css";
+
+export default function Quiz() {
+  const [triviaItems, setTriviaItems] = useState([]);
+
+  function checkResponseOk(res) {
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    return res;
+  }
+
+  useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&category=10&difficulty=easy&type=multiple"
+    )
+      .then((res) => checkResponseOk(res))
+      .then((res) => res.json())
+      .then((json) => toObjects(json))
+      .then((items) => setTriviaItems(items))
+      .catch((error) => console.log(error));
+  }, []);
+
+  function generateOptionsObjects(optionsArr) {
+    const randomizeArray = (arr) => arr.sort((a, b) => 0.5 - Math.random());
+    return randomizeArray(
+      optionsArr.map((optionText) => ({
+        id: nanoid(),
+        text: optionText,
+        selected: false,
+      }))
+    );
+  }
+
+  function setSelectedOption(itemId, optionId) {
+    const setSelected = (options) =>
+      options.map((option) =>
+        option.id === optionId
+          ? { ...option, selected: true }
+          : { ...option, selected: false }
+      );
+
+    setTriviaItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId
+          ? { ...item, options: setSelected(item.options, optionId) }
+          : item
+      )
+    );
+  }
+
+  function toObjects(response) {
+    return response.results.map((triviaItem) => ({
+      id: nanoid(),
+      question: decode(triviaItem.question),
+      options: generateOptionsObjects([
+        ...triviaItem.incorrect_answers,
+        triviaItem.correct_answer,
+      ]),
+      correctAnswer: triviaItem.correct_answer,
+      selected: false,
+    }));
+  }
+
+  const triviaItemElems = triviaItems.map((item) => (
+    <TriviaItem
+      key={item.id}
+      question={item.question}
+      options={item.options}
+      selected={item.selected}
+      setSelectedOption={(optionId) => setSelectedOption(item.id, optionId)}
+    />
+  ));
+
+  return (
+    <>
+      <div className="trivia-item-list">{triviaItemElems}</div>
+    </>
+  );
+}
