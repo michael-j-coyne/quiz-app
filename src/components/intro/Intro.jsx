c5c4cca36a4ed5baa991989ed2e81048321f8361
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-12 19:46:39 -0500

    Add classes, styling to Intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,13 @@
+import "./intro.css";
+
 export default function Intro() {
   return (
-    <>
-      <h1>Quiz yourself!</h1>
-      <p>Test your knowledge of various topics!</p>
-      <button>Start Quiz</button>
-    </>
+    <div className="intro">
+      <h1 className="intro__title">Quiz yourself!</h1>
+      <p className="intro__description">
+        Test your knowledge of various topics!
+      </p>
+      <button className="intro__button">Start Quiz</button>
+    </div>
   );
 }
