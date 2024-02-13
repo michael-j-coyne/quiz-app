181007a9c83fb7ea66441361585c32826e782118
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-13 15:36:20 -0500

    Use form in Quiz
    Change logic to accommodate the form, also changed TriviaItem to a form element as it is used in the Quiz.

--- src/components/trivia-item/TriviaItem.jsx
+++ src/components/trivia-item/TriviaItem.jsx
@@ -3,22 +3,47 @@ import "./trivia-item.css";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-export default function TriviaItem(props) {
-  const optionElems = props.options?.map((option) => (
-    <button
-      className={`trivia-item__option${
-        option.selected ? " trivia-item__option_selected" : ""
-      }`}
-      key={nanoid()}
-      onClick={() => props.setSelectedOption(option.id)}
-    >
-      {option.text}
-    </button>
-  ));
+function RadioInput({ value, labelText, checked, name, handleChange }) {
+  const id = nanoid();
 
   return (
-    <div className="question">
-      <h1 className="trivia-item__title">{props?.question || <Skeleton />}</h1>
+    <>
+      <input
+        type="radio"
+        name={name}
+        value={value}
+        id={id}
+        onChange={handleChange}
+        checked={checked}
+      />
+      <label htmlFor={id}>{labelText}</label>
+    </>
+  );
+}
+
+export default function TriviaItem(props) {
+  const optionElems = props.options?.map((optionText) => {
+    return (
+      <RadioInput
+        key={nanoid()}
+        value={optionText}
+        labelText={optionText}
+        name={props.questionId}
+        handleChange={props.handleChange}
+        checked={props.checkedOption === optionText}
+      />
+    );
+  });
+
+  return (
+    <fieldset className="question">
+      {props.skeleton ? (
+        <h1 className="trivia-item__title">
+          <Skeleton />
+        </h1>
+      ) : (
+        <legend className="trivia-item__title">{props.question}</legend>
+      )}
       <div className="trivia-item__options-container">
         {optionElems && optionElems}
         {props.skeleton && <Skeleton width={64} />}
@@ -27,6 +52,6 @@ export default function TriviaItem(props) {
         {props.skeleton && <Skeleton width={64} />}
       </div>
       <hr className="trivia-item__line"></hr>
-    </div>
+    </fieldset>
   );
 }
--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -6,6 +6,7 @@ import "./quiz.css";
 
 export default function Quiz() {
   const [triviaItems, setTriviaItems] = useState([]);
+  const [formData, setFormData] = useState([]); // { questionId: userAnswer }
   const numQuestions = 5;
 
   function checkResponseOk(res) {
@@ -15,6 +16,8 @@ export default function Quiz() {
     return res;
   }
 
+  const randomizeArray = (arr) => arr.sort((a, b) => 0.5 - Math.random());
+
   useEffect(() => {
     fetch(
       "https://opentdb.com/api.php?amount=5&category=10&difficulty=easy&type=multiple"
@@ -22,69 +25,59 @@ export default function Quiz() {
       .then((res) => checkResponseOk(res))
       .then((res) => res.json())
       .then((json) => toObjects(json))
-      .then((items) => setTriviaItems(items))
+      .then((obj_arr) => {
+        const newFormData = {};
+        obj_arr.forEach((obj) => (newFormData[obj.id] = ""));
+        setFormData(newFormData);
+
+        setTriviaItems(obj_arr);
+        return obj_arr;
+      })
       .catch((error) => console.log(error));
   }, []);
 
-  function generateOptionsObjects(optionsArr) {
-    const randomizeArray = (arr) => arr.sort((a, b) => 0.5 - Math.random());
-    return randomizeArray(
-      optionsArr.map((optionText) => ({
-        id: nanoid(),
-        text: optionText,
-        selected: false,
-      }))
-    );
-  }
+  function handleChange(event) {
+    const { name, value } = event.target;
 
-  function setSelectedOption(itemId, optionId) {
-    const setSelected = (options) =>
-      options.map((option) =>
-        option.id === optionId
-          ? { ...option, selected: true }
-          : { ...option, selected: false }
-      );
-
-    setTriviaItems((prevItems) =>
-      prevItems.map((item) =>
-        item.id === itemId
-          ? { ...item, options: setSelected(item.options, optionId) }
-          : item
-      )
-    );
-  }
-
-  function toObjects(response) {
-    return response.results.map((triviaItem) => ({
-      id: nanoid(),
-      question: decode(triviaItem.question),
-      options: generateOptionsObjects([
-        ...triviaItem.incorrect_answers,
-        triviaItem.correct_answer,
-      ]),
-      correctAnswer: triviaItem.correct_answer,
-      selected: false,
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
     }));
   }
 
-  const triviaItemElems = triviaItems?.map((item) => (
+  function toObjects(response) {
+    // What if there is no response.results?
+    return response.results.map((triviaItem) => ({
+      id: nanoid(),
+      question: decode(triviaItem.question),
+      options: randomizeArray([
+        ...triviaItem.incorrect_answers.map((ans) => decode(ans)),
+        decode(triviaItem.correct_answer),
+      ]),
+      answer: triviaItem.correct_answer,
+    }));
+  }
+
+  const triviaItemElems = triviaItems.map((item) => (
     <TriviaItem
       key={item.id}
-      isLoading={false}
+      questionId={item.id}
       question={item.question}
       options={item.options}
-      selected={item.selected}
-      setSelectedOption={(optionId) => setSelectedOption(item.id, optionId)}
+      checkedOption={formData[item.id]}
+      handleChange={handleChange}
     />
   ));
 
   return (
     <>
-      <div className="trivia-item-list">
+      <form className="trivia-item-list">
         {triviaItemElems.length > 0
           ? triviaItemElems
-          : new Array(numQuestions).fill(<TriviaItem skeleton={true} />)}
-      </div>
+          : new Array(numQuestions)
+              .fill(0)
+              .map(() => <TriviaItem key={nanoid()} skeleton={true} />)}
+      </form>
     </>
   );
 }
