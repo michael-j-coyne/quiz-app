4c9f7f97b12308edec3463c1f4777354913696cc
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-12 19:04:24 -0500

    Create basic Intro component

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,9 @@
+export default function Intro() {
+  return (
+    <>
+      <h1>Quiz yourself!</h1>
+      <p>Test your knowledge of various topics!</p>
+      <button>Start Quiz</button>
+    </>
+  );
+}
